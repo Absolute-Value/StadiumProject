12117f3729dde5fe5143f3c9ccf4a64ed369d2ea
Absolute-Value <ji.kei1220@gmail.com>
2022-07-18 21:27:37 +0900

    first commit

---
+++ Assets/Scripts/MoveBallCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBallCamera : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject camera;
+    private Vector3 offset;//カメラとの距離
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        InvokeRepeating("Timer", 0, 10);
+    }
+
+    void Update() {
+        if (this.transform.position.y < 0) {
+            CancelInvoke();
+            InvokeRepeating("Timer", 0, 10);
+        }
+        camera.transform.position = this.transform.position + offset;
+    }
+
+    void Timer()
+    {
+        transform.position = new Vector3(0, 1.5f, 0);
+        float force_x = -1.5f + 3 * Random.value;
+        float force_y = 1.0f + 2 * Random.value;
+        float force_z = 2.0f + 6 * Random.value;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(force_x, force_y, force_z, ForceMode.Impulse);
+
+        float nom = Mathf.Sqrt(Mathf.Pow(force_x, 2) + Mathf.Pow(force_z, 2)) + 0.0000001f;
+        offset =  new Vector3(-2*force_x / nom, 0.5f, -2*force_z / nom);
+        camera.transform.position = this.transform.position + offset;
+
+        float rad = Mathf.Atan2 (force_x, force_z);
+        camera.transform.rotation = Quaternion.Euler(10, rad * Mathf.Rad2Deg, 0);
+    }
+}
---
+++ Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    public GameObject mark;
+    private float z, addZ, alpha, beta;
+    private Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer();
+        InvokeRepeating("Timer", 0, 4);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        z += addZ;
+        addZ *= 0.999f;
+        this.transform.position = new Vector3(beta * z - 0.25f, -1 * alpha * z * z + 1.75f, -18 * z + 18.5f);
+        if (this.transform.position.z > 0.25f) {
+            mark.transform.position = new Vector3(beta * z - 0.25f, (0.5f - alpha) * z * z + 1.25f, 0.25f);
+        }
+    }
+
+    void Timer() {
+        z = 0;
+        alpha = 1 + 0.2f * Mathf.Sqrt(-2.0f * Mathf.Log(Random.value)) * Mathf.Sin(2.0f * Mathf.PI * Random.value);
+        beta = 0.2f * Mathf.Sqrt(-2.0f * Mathf.Log(Random.value)) * Mathf.Cos(2.0f * Mathf.PI * Random.value) + 0.25f;
+        addZ = getAdd(alpha);
+        this.transform.position = new Vector3(-0.25f, 1.75f, 18.5f);
+        mark.transform.position = new Vector3(-0.25f, 1.75f, 0.25f);
+    }
+
+    float getAdd(float _alpha) {
+        float velo;
+        if (alpha > 1.2f) {
+            velo = 105 + 15 * Random.value;
+        } else if (alpha < 0.8f) {
+            velo = 140 + 15 * Random.value;
+        } else {
+            velo = 120 + 20 * Random.value;
+        }
+        float take = 18.4f / (velo * 1000 / 3600);
+        return 0.02f / take;
+    }
+}
